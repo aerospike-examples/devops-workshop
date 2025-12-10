7c1e618899f57396fc483b4ca91b04bce42e5f1e
Zohar Elkayam <5897181+realmgic@users.noreply.github.com>
2025-12-10 15:53:04 +0200

    scripts to run asbench

--- asbench-configs/insert-1m-records.sh
+++ asbench-configs/insert-1m-records.sh
@@ -1,1 +1,1 @@
-asbench --workload-stages insert-1m-records.yaml --namespace devops --set testset -h aerospike-node-1
+asbench --workload-stages /home/aero_devops/asbench-configs/insert-1m-records.yaml --namespace devops --set testset -h aerospike-node-1
--- asbench-configs/read-write-workload.sh
+++ asbench-configs/read-write-workload.sh
@@ -1,1 +1,1 @@
-asbench --workload-stages read-write-workload.yaml --namespace devops --set testset -h aerospike-node-1
+asbench --workload-stages /home/aero_devops/asbench-configs/read-write-workload.yaml --namespace devops --set testset -h aerospike-node-1 
